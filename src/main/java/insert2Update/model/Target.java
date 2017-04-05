b20dff1e15aaef637871918002cdcf0550c1e0aa
Giampiero Di Paolo <dplgpr90@alice.it>
2017-04-05 20:34:39 +0200

    Project Configuration

---
+++ src/main/java/insert2Update/model/Target.java
@@ -0,0 +1,22 @@
+package model;
+
+public class Target {
+
+	public String schema;
+
+	public String table;
+
+	// Constructor
+	public Target(String schema, String table) {
+		this.schema = schema;
+		this.table = table;
+	}
+
+	public String toString() {
+		if (schema == null || schema.equals("")) {
+			return table;
+		}
+		return schema + "." + table;
+	}
+
+}
